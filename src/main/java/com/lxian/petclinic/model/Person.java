aa33b32a0c53b30d2433ee834b1e9d2b9fca508d
Darcy <Darcyxianau@gmail.com>
2020-07-13 20:34:51 +1000

    adding POJOs. Closes #2

---
+++ src/main/java/com/lxian/petclinic/model/Person.java
@@ -0,0 +1,27 @@
+package com.lxian.petclinic.model;
+
+/**
+ * @Author: Darcy Xian
+ * @Date: 2020/7/13  20:22
+ */
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
