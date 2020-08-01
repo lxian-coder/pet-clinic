f51f7abfb0a5c7d0ec3ed2d4a474cd1ca4b286db
Darcy <Darcyxianau@gmail.com>
2020-08-02 09:36:04 +1000

    added contact info for owner. added to vet. closes #29.

--- pet-clinic-data/src/main/java/com/lxian/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/lxian/petclinic/model/Owner.java
@@ -3,7 +3,35 @@ package com.lxian.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+
+    private String Address;
+    private String city;
+    private String telehpone;
     private Set<Pet> pets;
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelehpone() {
+        return telehpone;
+    }
+
+    public void setTelehpone(String telehpone) {
+        this.telehpone = telehpone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
--- pet-clinic-web/src/main/java/com/lxian/petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/lxian/petclinic/controllers/OwnersController.java
@@ -19,9 +19,10 @@ public class OwnersController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/","/index","/index.html",""})
-    public String listOwners(Model model){
-        model.addAttribute("owners",ownerService.findAll());
+    @RequestMapping({"/", "/index", "/index.html", ""})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+
     }
 }
