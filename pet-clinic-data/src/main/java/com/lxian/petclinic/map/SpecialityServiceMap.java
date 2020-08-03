2505fcaff086b89a08de969a1fafbaf594103610
Darcy <Darcyxianau@gmail.com>
2020-08-03 14:47:27 +1000

    added Speciality service and iml. Closes #32

---
+++ pet-clinic-data/src/main/java/com/lxian/petclinic/map/SpecialityServiceMap.java
@@ -0,0 +1,38 @@
+package com.lxian.petclinic.map;
+
+import com.lxian.petclinic.model.Speciality;
+import com.lxian.petclinic.services.SpecialityService;
+
+import java.util.Set;
+
+/**
+ * pet-clinic
+ * Author : Darcy Xian    Create : 2020-08-03  14:41
+ */
+
+public class SpecialityServiceMap extends AbstractService<Speciality,Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findByID(Long id) {
+        return super.findByID(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+       super.deleteByID(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/lxian/petclinic/services/SpecialityService.java
@@ -0,0 +1,10 @@
+package com.lxian.petclinic.services;
+
+import com.lxian.petclinic.model.Speciality;
+
+/**
+ * pet-clinic
+ * Author : Darcy Xian    Create : 2020-08-03  14:39
+ */
+public interface SpecialityService extends CrudService<Speciality,Long> {
+}
