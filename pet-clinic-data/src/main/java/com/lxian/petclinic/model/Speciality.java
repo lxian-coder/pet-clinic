cbd7a564a84b6843e57e011f548d8bdf65aba618
darcyxian <darcyxianau@gmail.com>
2020-08-11 12:01:11 +1000

    updated Vets to JPA entity mappings. Closes #38

--- pet-clinic-data/src/main/java/com/lxian/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/lxian/petclinic/model/Speciality.java
@@ -1,11 +1,17 @@
 package com.lxian.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * pet-clinic
  * Author : Darcy Xian    Create : 2020-07-30  13:50
  */
-
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
