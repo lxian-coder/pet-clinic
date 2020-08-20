400dcaaa73264bbf55f96f8377cc242a4d71e8d5
darcyxian <darcyxianau@gmail.com>
2020-08-20 15:10:50 +1000

    added tests for spring MVC test. Closes #53

---
+++ pet-clinic-web/src/main/java/test/OwnersControllerTest.java
@@ -0,0 +1,100 @@
+package test;
+
+import com.lxian.petclinic.controllers.OwnersController;
+import com.lxian.petclinic.model.Owner;
+import com.lxian.petclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Darcy Xian  20/8/20  2:08 pm      pet-clinic
+ */
+@ExtendWith(MockitoExtension.class)
+class OwnersControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnersController ownersController;
+
+    Set<Owner> owners;
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1l).build());
+        owners.add(Owner.builder().id(2l).build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(ownersController)
+                .build();
+    }
+
+    @Test
+    void listOwners() throws Exception {
+      when(ownerService.findAll()).thenReturn(owners);
+      mockMvc.perform(get("/owners"))
+              .andExpect(status().isOk())
+              .andExpect(view().name("owners/index"))
+              .andExpect(model().attribute("owners",hasSize(2)));
+    }
+
+    @Test
+    void findOwners() throws Exception {
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("notImplement"));
+        verifyNoInteractions(ownerService);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
