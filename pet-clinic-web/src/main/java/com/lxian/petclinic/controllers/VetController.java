fa69cc3717d842c0b3787578481caf175931265b
darcyxian <darcyxianau@gmail.com>
2020-09-15 21:51:52 +1000

    added json endpoint. closes #69

--- pet-clinic-web/src/main/java/com/lxian/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/lxian/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.lxian.petclinic.controllers;
 
+import com.lxian.petclinic.model.Vet;
 import com.lxian.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * pet-clinic
@@ -23,6 +28,12 @@ public class VetController {
         model.addAttribute("vets",vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Iterable<Vet> getVetsJson(){
+
+         return  vetService.findAll();
     }
 
 }
